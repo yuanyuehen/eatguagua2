6f2254f55f3f200c7003373b8ff5cafbfc324d53
yuanyuehen <49892658+yuanyuehen@users.noreply.github.com>
2021-01-31 23:40:04 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -6,7 +6,7 @@
 let extraScore = 1;
 
 // 无敌模式：true 改为 false
-let wuDi = true;
+let wuDi = false;
 
 // 第一个水果：修改数字为 0-10
 let firstFruit = 0;
@@ -24,7 +24,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = 0.5;
+let fruitQTan = 0.3;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
