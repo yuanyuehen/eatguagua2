73a6c6c861d254a0c3a638915924466f62079b42
yuanyuehen <49892658+yuanyuehen@users.noreply.github.com>
2021-02-01 16:49:23 +0800

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -17,14 +17,14 @@ let firstFruit = 0;
 const minRandomFruitNum = 0; // 生成随机水果最小值（0-10）
 const maxRandomFruitNum = 5; // 生成随机水果最大值（1-11）
 let setFruits = {
-  startFruits: [0, 0, 1, 2, 3, 4], // 指定前几次生成的水果，可填入任意数量的数字
+  startFruits: [0, 0, 1, 2, 3, 3], // 指定前几次生成的水果，可填入任意数量的数字
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = 0.5;
+let fruitQTan = false;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
